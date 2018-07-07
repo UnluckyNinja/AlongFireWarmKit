963833ffccd8378386d2aaf7faebb3b4d3dc9c43
UnluckyNinja <unluckyninja1994@gmail.com>
2018-07-08 04:51:43 +0800

    clean content

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<div id="app">
+  <h1><router-link to="/" class="title">Title</router-link></h1>
+  <router-view/>
+  <div id="footer">
+    <router-link to="/about">About</router-link>
   </div>
+</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+.title {
+  padding: 10px;
+  border-radius: 5px;
+  color: inherit;
+  text-decoration: inherit;
+}
+#footer {
   padding: 30px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
   a {
     font-weight: bold;
     color: #2c3e50;
